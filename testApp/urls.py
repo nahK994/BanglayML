2e9082781658f179eaa66d262cad352635864ee5
ShomiKhan <nkskl6@gmail.com>
2019-10-08 23:11:27 +0600

    Introduction & RL 1.0

--- testApp/urls.py
+++ testApp/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('SL_View/', views.SL_View.as_view(), name="SL_View"),
     path('UL_View/', views.UL_View.as_view(), name="UL_View"),
     path('RL_View/', views.RL_View.as_view(), name="RL_View"),
-    path('IntroText/', views.IntroText.as_view(), name="IntroText"),
+    path('AI_IntroText/', views.AI_IntroText.as_view(), name="AI_IntroText"),
+    path('ML_IntroText/', views.ML_IntroText.as_view(), name="ML_IntroText"),
+    path('RL_IntroText/', views.RL_IntroText.as_view(), name="RL_IntroText"),
+    path('RL_Q_learningText/', views.RL_Q_learningText.as_view(), name="RL_Q_learningText")
 ]
--- testApp/views.py
+++ testApp/views.py
@@ -12,5 +12,14 @@ class UL_View(TemplateView):
 class RL_View(TemplateView):
     template_name = "RL_View.html"
 
-class IntroText(TemplateView):
-    template_name = "IntroText.html"
+class AI_IntroText(TemplateView):
+    template_name = "AI_IntroText.html"
+
+class ML_IntroText(TemplateView):
+    template_name = "ML_IntroText.html"
+
+class RL_IntroText(TemplateView):
+    template_name = "RL_IntroText.html"
+
+class RL_Q_learningText(TemplateView):
+    template_name = "RL_Q_learningText.html"
