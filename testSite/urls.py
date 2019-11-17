057db835707de3607aa79d490dcae0191955626e
ShomiKhan <nkskl6@gmail.com>
2019-11-17 17:10:01 +0600

    testSite/urlpattern updated

--- testSite/urls.py
+++ testSite/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', include('testApp.urls')),
 ]
 
-if settings.DEBUG: # new
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
